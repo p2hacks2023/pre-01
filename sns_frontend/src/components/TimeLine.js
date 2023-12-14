315bdf24ea9db02c7eb57b90ca5f6a73aeb44268
mikinakafun <120154817+mikinakafun@users.noreply.github.com>
2023-12-14 18:04:30 +0900

    add timeline head
    タイムラインのプロフィール大まかなデザイン完成

--- sns_frontend/src/components/TimeLine.js
+++ sns_frontend/src/components/TimeLine.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import '../css/TimeLine.scss'
+import testImage from '../images/logo192.png'
 
 function TimeLine(props) {
     const [isButton, setIsButton] = useState(true);
@@ -37,8 +38,10 @@ function TimeLine(props) {
                         <div className='timeline-Profile'>
                             {/*プロフィール画像*/}
                             <div className='profile-Img-Frame'>
-                                <img className='profile-Img'></img>
+                                <img src={testImage} className="profile-Img" alt="logo" />
+                                {/* <img className='profile-Img'></img> */}
                             </div>
+                            {/*プロフィール右側*/}
                             <div className='profile-Right'>
                                 {/*名前*/}
                                 <div className='profile-Name-Frame'>
@@ -46,7 +49,19 @@ function TimeLine(props) {
                                 </div>
                                 {/*フォローボタン*/}
                                 <div className='profile-FollowB-Frame'>
-                                    <button className='proprofile-FollowB'>Follow</button>
+                                    <button className='profile-FollowB'>Follow</button>
+                                </div>
+                                {/*いいね数*/}
+                                <div className="sweets-frame">
+                                    <div className="sweets-Head">Sweets</div>
+                                    <div className="sweets-Number">89</div>
+                                </div>
+                            </div>
+                            {/*プロフィール下部分*/}
+                            <div className='profile-Foot'>
+                                {/*動物種類テキスト*/}
+                                <div className="animalS-Frame">
+                                    <div className='animalS'>Penguin</div>
                                 </div>
                             </div>
                         </div>
